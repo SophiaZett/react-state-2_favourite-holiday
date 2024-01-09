18297130124cf78cf22e0d1d6196caf85465471c
Sophia S. <sophia0791@icloud.com>
2024-01-09 18:58:39 +0100

    solve task & bonus

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+  const holidayInputref = useRef(null); //(bonus aufgabe) wir brauchen eine referenz (die useRef - Hook)
+
   function handleSubmit(event) {
     event.preventDefault();
+    const holidayValue = event.target.holiday.value;
+    const dateValue = event.target.date.value;
+    setHoliday(holidayValue);
+    setDate(dateValue);
+
+    // (bonus aufgabe)Zurücksetzen der Formularfelder
+    setHoliday("");
+    setDate("");
+
+    // (bonus aufgabe) Fokus auf das Holiday-Feld. bzgl current: .current ist der Teil der Referenz, der tatsächlich auf das Element selbst zugreift.
+    holidayInputref.current.focus();
   }
 
   return (
@@ -17,23 +32,32 @@ export default function App() {
       >
         <label htmlFor="holiday">My Favourite Holiday: </label>
         <input
+          ref={holidayInputref} // (bonus aufgabe) verknüpfung der referenz mit dem holiday-feld
           id="holiday"
           type="text"
           name="holiday"
           placeholder="e.g. Christmas"
+          value={holiday} //(bonus aufgabe)
+          onChange={(event) => setHoliday(event.target.value)} //(bonus aufgabe)
         />
         <label htmlFor="date">Date: </label>
-        <input id="date" type="date" name="date" />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          value={date} //(bonus aufgabe)
+          onChange={(event) => setDate(event.target.value)} //(bonus aufgabe)
+        />
         <button type="submit" className="form__submit-button">
           Submit
         </button>
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
